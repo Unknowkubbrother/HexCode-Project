74070890d13f25235a22b548f3d22e594d56cb80
Natchkung <natchnon654321@gmail.com>
2024-12-19 01:25:36 +0700

    update theme color

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <html lang="en">
+      <html lang="en" data-theme="dark">
         <body className={inter.className}>
           <header>
             <Navbar />
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: [
@@ -9,10 +10,58 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        darkbgprimary: "var(--dark-bg-primary)",
+        darkbgsecondary: "var(--dark-bg-secondary)",
+        lightbgprimary: "var(--light-bg-primary)",
+        lightbgsecondary: "var(--light-bg-secondary)"
       },
+      extend: {},
     },
   },
-  plugins: [],
+  daisyui: {
+    base: false, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ':root', // The element that receives theme color CSS variables,
+    themes: [
+      {
+        mytheme: {
+          "primary": 'rgb(77, 181, 255)',
+  
+          "secondary": 'rgb(40, 139, 209)',
+  
+          "accent": '#0069a9',
+  
+          "neutral": '#3d4451',
+  
+          'base-100': '#262626',
+  
+          "info": '#00e2ff',
+  
+          "success": '#00cc99',
+  
+          "warning": '#f47000',
+  
+          "error": '#d2004e'
+        },
+        dark : {
+          primary: 'var(--color-primary)',
+          secondary: 'var(--color-secondary)',
+          bgprimary: 'var(--dark-bg-primary)',
+          bgsecondary: 'var(--dark-bg-secondary)',
+        },
+        light :{
+          primary: 'var(--color-primary)',
+          secondary: 'var(--color-secondary)',
+          bgprimary: 'var(--light-bg-primary)',
+          bgsecondary: 'var(--light-bg-secondary)',
+        }
+      },
+    ],
+  },
+  plugins: [daisyui],
 } satisfies Config;
