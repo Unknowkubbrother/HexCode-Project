27a514943730a882fdb2028bf8d57eafa116c63f
Natchkung <natchnon654321@gmail.com>
2024-12-22 03:44:54 +0700

    update last

--- client/app/action.ts
+++ client/app/action.ts
@@ -1,12 +1,15 @@
 'use server';
 import axios from 'axios';
-// import { currentUser } from '@clerk/nextjs/server'
 import {cookies} from "next/headers";
 
 export const getProblem = async () => {
     try{
         const cookieStore = await cookies()
         const token = cookieStore.get('__session')
+
+        if (!token) {
+            throw new Error('Unauthorized');
+        }
 
         const response = await axios.get('http://localhost:4000/problem/get', {
             headers: { Authorization: `Bearer ${token?.value}` },
