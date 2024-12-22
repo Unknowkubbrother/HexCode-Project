a993b56378bbf17d15fc46aa79fe28581caf277a
Natchkung <natchnon654321@gmail.com>
2024-12-23 02:07:14 +0700

    update not found problem

--- client/app/(problems)/problems/_components/ListProblems.tsx
+++ client/app/(problems)/problems/_components/ListProblems.tsx
@@ -3,19 +3,23 @@ import ItemProblem from "./ItemProblem";
 import { ProblemInterface } from "@/interface/problems";
 
 interface Props {
-  result : [];
+  result: [];
 }
 
-export default async function ListProblems({result} : Props) {
-
-
-    return (
+export default async function ListProblems({ result }: Props) {
+  return (
     <div className="w-full h-fit max:h-[2000px] overflow-y-auto">
-      <div className="w-full h-fit grid grid-cols-1 gap-3">
-        {result.map((value: ProblemInterface) => (
-          <ItemProblem value={value} key={value.id} />
-        ))}
-      </div>
+      {result.length > 0 ? (
+        <div className="w-full h-fit grid grid-cols-1 gap-3">
+          {result.map((value: ProblemInterface) => (
+            <ItemProblem value={value} key={value.id} />
+          ))}
+        </div>
+      ) : (
+        <div className="w-full h-fit flex justify-center items-center">
+          <span className="text-lg font-bold">No problems found</span>
+        </div>
+      )}
     </div>
   );
 }
