22c291ceba7f5176556444c30ea28b11e164bb2f
unknown <siwakchuhirun2004@gmail.com>
2024-12-22 19:04:12 +0700

    edit page

--- server/src/router/Problems.ts
+++ server/src/router/Problems.ts
@@ -65,68 +65,25 @@ export const ProblemRoute = new Elysia({ prefix: "/problem" })
 
         // const user = await clerk.users.getUser(auth.userId)
 
-        const sizepage = Number(query.pagesize)||10;
-        const page = Number(query.page)||1;
-
-        const problems = await ProblemModel.find().skip(sizepage*(page-1)).limit(sizepage);
-
-        const countpage = await ProblemModel.countDocuments({})
-        const numberpage: number = ceil(countpage / sizepage);
-
-        const resultProblem = await Promise.all(problems.map(async (value, idx) => {
-          const userbyid = await clerk.users.getUser(value.clerkId)
-          return {
-            id: value._id.toString(),
-            title: value.title,
-            difficulty: value.difficulty,
-            submissions: value.submissions,
-            accepted: value.accepted,
-            successRate: (value.accepted / value.submissions) * 100 || 0,
-            clid: value.clerkId,
-            author: {
-              name: `${userbyid.username}`,
-              avatar: userbyid.imageUrl
-            },
-            point: value.point,
-          };
-        }))
-        return {
-          result: resultProblem,
-          numberpage: {
-            page: page,
-            all: numberpage,
-          },
-        };
-      } catch (e) {
-        return error(500, "Internal Server Error");
-      }
-    }
-  )
-
-  .post(
-    "/get",
-    async ({ body, clerk, auth, error }) => {
-      try {
+        const sizepage = query?.pagesize?query.pagesize:10;
+        const page = query?.page?query.page:1;
+        const difficulty = query.difficulty?JSON.parse(query.difficulty):""
+        const type = query.type?JSON.parse(query.type):""
         
-        // if (!auth?.userId){
-        //    return error(401, 'Unauthorized')
-        // }
-
-        // const user = await clerk.users.getUser(auth.userId)
-
-        const sizepage = body?.pagesize?body.pagesize:10;
-        const page = body?.page?body.page:1;
-
-        const problems = await ProblemModel.find(
-          body?.difficulty?{difficulty:{$in:body.difficulty}}:{},
-          body?.type?{type:{$in:body.difficulty}}:{}
+        const problems = await ProblemModel.find({
+          $and:[
+            difficulty?{difficulty:{$in :difficulty}}:{},
+            type!=""?{type:{$in:type}}:{}
+          ]
+        }
         ).skip(sizepage*(page-1)).limit(sizepage);
-
         const countpage = await ProblemModel.countDocuments(
-          body?.difficulty?{difficulty:{$in:body.difficulty}}:{},
-          body?.type?{type:{$in:body.difficulty}}:{})
+          {$and:[
+            difficulty?{difficulty:{$in :difficulty}}:{},
+            type!=""?{type:{$in:type}}:{}
+          ]}
+        )
         const numberpage: number = ceil(countpage / sizepage);
-
         const resultProblem = await Promise.all(problems.map(async (value, idx) => {
           const userbyid = await clerk.users.getUser(value.clerkId)
           return {
@@ -137,6 +94,7 @@ export const ProblemRoute = new Elysia({ prefix: "/problem" })
             accepted: value.accepted,
             successRate: (value.accepted / value.submissions) * 100 || 0,
             clid: value.clerkId,
+            type:value.type,
             author: {
               name: `${userbyid.username}`,
               avatar: userbyid.imageUrl
@@ -156,15 +114,81 @@ export const ProblemRoute = new Elysia({ prefix: "/problem" })
       }
     },
     {
-      body: t.Optional(
+      query: t.Optional(
         t.Object({
           page: t.Optional(t.Number()),
           pagesize: t.Optional(t.Number()),
           slove: t.Optional(t.Boolean()),
           unslove: t.Optional(t.Boolean()),
-          type: t.Optional(t.Array(t.String())),
-          difficulty: t.Optional(t.Array(t.Number())),
+          type: t.Optional(t.String()),
+          difficulty: t.Optional(t.String()),
         })
       ),
     }
-  );
+  )
+
+  // .post(
+  //   "/get",
+  //   async ({ body, clerk, auth, error }) => {
+  //     try {
+        
+  //       // if (!auth?.userId){
+  //       //    return error(401, 'Unauthorized')
+  //       // }
+
+  //       // const user = await clerk.users.getUser(auth.userId)
+
+  //       const sizepage = body?.pagesize?body.pagesize:10;
+  //       const page = body?.page?body.page:1;
+
+  //       const problems = await ProblemModel.find(
+  //         body?.difficulty?{difficulty:{$in:body.difficulty}}:{},
+  //         body?.type?{type:{$in:body.difficulty}}:{}
+  //       ).skip(sizepage*(page-1)).limit(sizepage);
+
+  //       const countpage = await ProblemModel.countDocuments(
+  //         body?.difficulty?{difficulty:{$in:body.difficulty}}:{},
+  //         body?.type?{type:{$in:body.difficulty}}:{})
+  //       const numberpage: number = ceil(countpage / sizepage);
+
+  //       const resultProblem = await Promise.all(problems.map(async (value, idx) => {
+  //         const userbyid = await clerk.users.getUser(value.clerkId)
+  //         return {
+  //           id: value._id.toString(),
+  //           title: value.title,
+  //           difficulty: value.difficulty,
+  //           submissions: value.submissions,
+  //           accepted: value.accepted,
+  //           successRate: (value.accepted / value.submissions) * 100 || 0,
+  //           clid: value.clerkId,
+  //           author: {
+  //             name: `${userbyid.username}`,
+  //             avatar: userbyid.imageUrl
+  //           },
+  //           point: value.point,
+  //         };
+  //       }))
+  //       return {
+  //         result: resultProblem,
+  //         numberpage: {
+  //           page: page,
+  //           all: numberpage,
+  //         },
+  //       };
+  //     } catch (e) {
+  //       return error(500, "Internal Server Error");
+  //     }
+  //   },
+  //   {
+  //     body: t.Optional(
+  //       t.Object({
+  //         page: t.Optional(t.Number()),
+  //         pagesize: t.Optional(t.Number()),
+  //         slove: t.Optional(t.Boolean()),
+  //         unslove: t.Optional(t.Boolean()),
+  //         type: t.Optional(t.Array(t.String())),
+  //         difficulty: t.Optional(t.Array(t.Number())),
+  //       })
+  //     ),
+  //   }
+  // );
