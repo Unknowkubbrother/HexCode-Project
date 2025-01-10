451254950216318b6db1de88097ebef277011be2
Unknowkubbrother <natchnon654321@gmail.com>
2025-01-11 02:11:15 +0700

    test pdfviewer deploy vercel

--- client/components/ui/pdf-viewer.tsx
+++ client/components/ui/pdf-viewer.tsx
@@ -4,13 +4,11 @@ import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import { cn } from "@/lib/utils";
-import { themePlugin } from '@react-pdf-viewer/theme';
 import { useTheme } from "next-themes";
 
 export default function PDFViewer(props: { url: string; className?: string }) {
     const { url } = props;
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
-    const themePluginInstance = themePlugin();
     const { theme } = useTheme();
 
     return (
@@ -18,7 +16,7 @@ export default function PDFViewer(props: { url: string; className?: string }) {
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
                 <Viewer
                     fileUrl={url}
-                    plugins={[defaultLayoutPluginInstance,themePluginInstance]}
+                    plugins={[defaultLayoutPluginInstance]}
                     withCredentials={true}
                     theme={theme}
                 />
