466993f9fa9253e0c157492d18f7513ed06c3f67
Unknowkubbrother <natchnon654321@gmail.com>
2025-01-11 02:49:08 +0700

    fix auth file pdfviewer

--- client/components/ui/pdf-viewer.tsx
+++ client/components/ui/pdf-viewer.tsx
@@ -1,26 +1,36 @@
 "use client";
+import { useEffect, useState } from "react";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import { cn } from "@/lib/utils";
 import { useTheme } from "next-themes";
+import getSession from "@/hooks/use-session";
 
 export default function PDFViewer(props: { url: string; className?: string }) {
-    const { url } = props;
-    const defaultLayoutPluginInstance = defaultLayoutPlugin();
-    const { theme } = useTheme();
+  const { url } = props;
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const { theme } = useTheme();
+  const [token, setToken] = useState<string>("");
 
-    return (
-        <div className={cn("h-full w-full", props.className)}>
-            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-                <Viewer
-                    fileUrl={url}
-                    plugins={[defaultLayoutPluginInstance]}
-                    withCredentials={true}
-                    theme={theme}
-                />
-            </Worker>
-        </div>
-    );
+  useEffect(() => {
+    getSession().then((result) => setToken(result));
+  }, []);
+
+  return (
+    <div className={cn("h-full w-full", props.className)}>
+      {token && (
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+          <Viewer
+            fileUrl={url}
+            httpHeaders={{ Authorization: `Bearer ${token}` }}
+            plugins={[defaultLayoutPluginInstance]}
+            withCredentials={true}
+            theme={theme}
+          />
+        </Worker>
+      )}
+    </div>
+  );
 }
