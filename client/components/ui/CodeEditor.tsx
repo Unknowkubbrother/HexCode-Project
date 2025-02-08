17cb7c80c2068d34be794f9cb182a890e48a287d
Unknowkubbrother <natchnon654321@gmail.com>
2025-02-09 00:38:46 +0700

    test`1

--- client/components/ui/CodeEditor.tsx
+++ client/components/ui/CodeEditor.tsx
@@ -14,7 +14,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
 
 export default function CodeEditor(props: {
   code: string;
