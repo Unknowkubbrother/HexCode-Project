87975aa250d5fe12882a0d3bb14e438d8323be47
Unknowkubbrother <natchnon654321@gmail.com>
2025-02-16 02:07:30 +0700

    update serach

---
+++ server/lib/resendEmail.ts
@@ -0,0 +1,14 @@
+import { Resend } from 'resend';
+
+export const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendNotification = async (email: string, subject: string, html: string) => {
+    if (process.env.NODE_ENV === 'development') return;
+
+    resend.emails.send({
+        from: 'noreply@email.unknowkubbrother.net',
+        to: email,
+        subject: 'HEXCODE - ' + subject,
+        html: html,
+    });
+}
