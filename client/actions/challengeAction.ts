8986c474150bb729d7e87d821459d66cc7fb99f9
Unknowkubbrother <natchnon654321@gmail.com>
2025-03-19 01:01:17 +0700

    fixbug

--- client/actions/challengeAction.ts
+++ client/actions/challengeAction.ts
@@ -3,25 +3,6 @@ import axios from 'axios';
 import getSession from "@/hooks/use-session";
 import { redirect } from 'next/navigation'
 
-export const getChallengesProblemById = async (id: string) => {
-    try{
-        const token = await getSession();
-
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_END_POINT}/problem/get/${id}`, {
-            headers: { Authorization: `Bearer ${token}` },
-        });
-
-        if (!response) {
-            throw new Error('Error');
-        }
-
-
-        return response.data;
-    }catch(error){
-        console.log(error);
-    }
-};
-
 export const getChallenges = async () : Promise<any> => {
     try{
         const token = await getSession();
