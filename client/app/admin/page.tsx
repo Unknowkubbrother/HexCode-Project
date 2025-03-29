31bac6071b7f7dfbe32ca5988c836b8f226a4be1
Unknowkubbrother <natchnon654321@gmail.com>
2025-03-30 03:06:16 +0700

    fix dynamic force admin

--- client/app/admin/page.tsx
+++ client/app/admin/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
 import AdminHome from "./_components/AdminHome";
 import { getMyAccount } from "@/actions/profileAction";
